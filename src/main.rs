1c05fd9a2f4d2c5bf7c281c40d38bf71e02fe4bc
Tereschenkov Artem <Tereschenkov.A@citilink.ru>
2023-08-22 18:55:14 +0600

    day2 part1

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
 mod day1;
+mod day2;
 
 fn main() {
-    // match day1::part1::find_max_calories() {
-    //     Ok(max_total) => { println!("Maximum total calories is: {}", max_total); }
-    //     Err(e) => eprintln!("Error: {}", e),
-    // }
+    match day1::part1::find_max_calories() {
+        Ok(max_total) => { println!("Maximum total calories is: {}", max_total); }
+        Err(e) => eprintln!("Error: {}", e),
+    }
 
     match day1::part2::find_top_three_calories_sum() {
         Ok(top_three_sum) => { println!("Sum of top three calories is: {}", top_three_sum); }
+        Err(e) => eprintln!("Error: {}", e),
+    }
+
+    match day2::part1::find_total_scores() {
+        Ok(scores) => { println!("Sum of top three calories is: {}", scores); }
         Err(e) => eprintln!("Error: {}", e),
     }
 }
